b3e4e7689fd1b2ee342ac0d31f3f72622ab9e79c
Kotaro7750 <7750koutarou@gmail.com>
2020-03-13 18:11:07 +0900

    header

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,28 +1,28 @@
 <template>
 <div>
-      <b-navbar toggleable="lg" type="dark" variant="dark">
-        <b-navbar-brand href="/">ePortfolio</b-navbar-brand>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="/">ePortfolio</b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav>
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item href="/purchase">株式購入</b-nav-item>
-            <b-nav-item href="/ticker">ティッカー</b-nav-item>
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <template v-slot:button-content>
+            <em>資産</em>
+          </template>
+          <b-dropdown-item href="/">資産</b-dropdown-item>
+        </b-nav-item-dropdown>
 
-            <b-nav-item-dropdown right>
-              <template v-slot:button-content>
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item @click="signOut">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
-            <b-nav-item @click="signOut">サインアウト</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
+        <b-nav-item href="/purchase">株式購入</b-nav-item>
+        <b-nav-item href="/ticker">ティッカー</b-nav-item>
+
+        <b-nav-item @click="signOut">サインアウト</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
 </template>
 
 <script>
