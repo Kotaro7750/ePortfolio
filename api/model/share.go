ab86f72d134a0e8256bb83a3e0e849e8de2962f1
Kotaro7750 <7750koutarou@gmail.com>
2020-03-19 17:18:44 +0900

    color of ticker

--- api/model/share.go
+++ api/model/share.go
@@ -10,6 +10,7 @@ import (
 type Share struct {
 	Id        int     `json:"id"`
 	Ticker    string  `json:"ticker"`
+	Color     string  `json:"color"`
 	Amount    int     `json:"amount"`
 	Dividened float64 `json:"dividened"`
 	TotalCost float64 `json:"total_cost"`
@@ -18,7 +19,7 @@ type Share struct {
 
 func GetShareList(db *sql.DB) ([]Share, error) {
 	rows, err := db.Query(
-		"SELECT ticker.id,ticker.ticker,ticker.dividened,purchase_history.share, purchase_history.cost FROM purchase_history INNER JOIN ticker ON purchase_history.ticker_id = ticker.id")
+		"SELECT ticker.id,ticker.ticker,ticker.color,ticker.dividened,purchase_history.share, purchase_history.cost FROM purchase_history INNER JOIN ticker ON purchase_history.ticker_id = ticker.id")
 
 	if err != nil {
 		log.Fatal(err)
@@ -28,6 +29,7 @@ func GetShareList(db *sql.DB) ([]Share, error) {
 	var shares []Share
 	tmp := make(map[int]struct {
 		ticker    string
+		color     string
 		amount    int
 		dividened float64
 		totalCost float64
@@ -36,11 +38,12 @@ func GetShareList(db *sql.DB) ([]Share, error) {
 	for rows.Next() {
 		var id int
 		var ticker string
+		var color string
 		var amount int
 		var dividened float64
 		var totalCost float64
 
-		if err := rows.Scan(&id, &ticker, &dividened, &amount, &totalCost); err != nil {
+		if err := rows.Scan(&id, &ticker, &color, &dividened, &amount, &totalCost); err != nil {
 			log.Printf("Query Error: %s", err.Error())
 			return nil, err
 		}
@@ -49,11 +52,13 @@ func GetShareList(db *sql.DB) ([]Share, error) {
 
 		tmp[id] = struct {
 			ticker    string
+			color     string
 			amount    int
 			dividened float64
 			totalCost float64
 		}{
 			ticker:    ticker,
+			color:     color,
 			amount:    amount,
 			dividened: dividened,
 			totalCost: totalCost,
@@ -70,7 +75,7 @@ func GetShareList(db *sql.DB) ([]Share, error) {
 		meanCost := obj.totalCost / float64(obj.amount)
 		meanCost, _ = strconv.ParseFloat(fmt.Sprintf("%.2f", meanCost), 64)
 
-		shares = append(shares, Share{Id: id, Ticker: obj.ticker, Amount: obj.amount, Dividened: totalDividened, TotalCost: obj.totalCost, MeanCost: meanCost})
+		shares = append(shares, Share{Id: id, Ticker: obj.ticker, Color: obj.color, Amount: obj.amount, Dividened: totalDividened, TotalCost: obj.totalCost, MeanCost: meanCost})
 	}
 	return shares, nil
 }
--- api/model/ticker.go
+++ api/model/ticker.go
@@ -14,10 +14,11 @@ type Ticker struct {
 	SectorId  int     `json:"sector_id"`
 	Sector    string  `json:"sector"`
 	AboutURL  string  `json:"about_url"`
+	Color     string  `json:"color"`
 }
 
 func GetTickerList(db *sql.DB) ([]Ticker, error) {
-	rows, err := db.Query("SELECT ticker.id, ticker.ticker,ticker.dividened,ticker.sector,sector.sector, ticker.about_url FROM ticker INNER JOIN sector ON ticker.sector = sector.id")
+	rows, err := db.Query("SELECT ticker.id, ticker.ticker,ticker.dividened,ticker.sector,sector.sector, ticker.about_url, ticker.color FROM ticker INNER JOIN sector ON ticker.sector = sector.id")
 
 	if err != nil {
 		log.Fatal(err)
@@ -28,7 +29,7 @@ func GetTickerList(db *sql.DB) ([]Ticker, error) {
 
 	for rows.Next() {
 		var ticker Ticker
-		if err := rows.Scan(&ticker.Id, &ticker.Ticker, &ticker.Dividened, &ticker.SectorId, &ticker.Sector, &ticker.AboutURL); err != nil {
+		if err := rows.Scan(&ticker.Id, &ticker.Ticker, &ticker.Dividened, &ticker.SectorId, &ticker.Sector, &ticker.AboutURL, &ticker.Color); err != nil {
 			log.Printf("Query Error: %s", err.Error())
 			return nil, err
 		}
@@ -43,13 +44,13 @@ func GetTickerList(db *sql.DB) ([]Ticker, error) {
 }
 
 func AddTicker(db *sql.DB, t Ticker) error {
-	ins, err := db.Prepare("INSERT INTO ticker (ticker,dividened,sector,about_url) VALUES ($1,$2,$3,$4)")
+	ins, err := db.Prepare("INSERT INTO ticker (ticker,dividened,sector,about_url,color) VALUES ($1,$2,$3,$4,$5)")
 	if err != nil {
 		log.Printf("Query Error: %s", err.Error())
 		return err
 	}
 
-	_, err = ins.Exec(t.Ticker, t.Dividened, t.SectorId, t.AboutURL)
+	_, err = ins.Exec(t.Ticker, t.Dividened, t.SectorId, t.AboutURL, t.Color)
 	if err != nil {
 		log.Printf("Exec Error: %s", err.Error())
 		return err
@@ -59,13 +60,13 @@ func AddTicker(db *sql.DB, t Ticker) error {
 }
 
 func UpdateTicker(db *sql.DB, t Ticker) error {
-	ins, err := db.Prepare("UPDATE ticker SET ticker=$1,dividened=$2,sector=$3,about_url=$4 WHERE id=$5")
+	ins, err := db.Prepare("UPDATE ticker SET ticker=$1,dividened=$2,sector=$3,about_url=$4,color=$5 WHERE id=$6")
 	if err != nil {
 		log.Printf("Query Error: %s", err.Error())
 		return err
 	}
 
-	_, err = ins.Exec(t.Ticker, t.Dividened, t.SectorId, t.AboutURL, t.Id)
+	_, err = ins.Exec(t.Ticker, t.Dividened, t.SectorId, t.AboutURL, t.Color, t.Id)
 	if err != nil {
 		log.Printf("Exec Error: %s", err.Error())
 		return err
